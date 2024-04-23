45c6ed68601768e42d6998a8964fe094d496fcba
Flow <highflowey@gmail.com>
2024-04-23 05:18:20 +0330

    use the new static variables instead of app data

--- src/lune/builtins/webview/window.rs
+++ src/lune/builtins/webview/window.rs
@@ -1,7 +1,7 @@
 use std::{rc::Weak, time::Duration};
 
 use mlua::prelude::*;
-use mlua_luau_scheduler::{LuaSchedulerExt, LuaSpawnExt};
+use mlua_luau_scheduler::LuaSpawnExt;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
@@ -10,10 +10,10 @@ use winit::{
 };
 use wry::WebViewBuilder;
 
-use super::{enums::LuaWindowEvent, WEBVIEWS, WINDOWS};
+use super::{enums::LuaWindowEvent, EVENT_LOOP, WEBVIEWS, WINDOWS};
 
 pub struct LuaWindow {
-    window_id: WindowId,
+    pub window_id: WindowId,
 }
 
 impl LuaWindow {
@@ -50,119 +50,115 @@ impl LuaWindow {
         };
 
         let id = window.id();
-        WEBVIEWS.with_borrow_mut(|map| map.insert(id.clone(), webview.build().unwrap()));
-        WINDOWS.with_borrow_mut(|map| map.insert(id.clone(), window));
+        WEBVIEWS.with_borrow_mut(|map| {
+            let webview = webview.build().unwrap();
+            map.insert(id, webview)
+        });
+        WINDOWS.with_borrow_mut(|map| map.insert(id, window));
 
         Ok(LuaWindow { window_id: id })
     }
 }
 
-fn lua_window_process_events<'lua>(lua: &'lua Lua, callback: LuaFunction<'lua>) -> LuaResult<()> {
-    let func = lua.create_async_function(|lua: &Lua, callback: LuaFunction| async move {
-        let mut event_loop = lua.app_data_mut::<EventLoop<()>>().unwrap();
-        // let (send, mut receive) = channel::<LuaWindowEvent>(LuaWindowEvent::Nothing);
+async fn lua_window_process_events<'lua>(
+    _lua: &'lua Lua,
+    this: &LuaWindow,
+    callback: LuaFunction<'lua>,
+) -> LuaResult<()> {
+    loop {
+        let mut lua_window_event: Option<LuaWindowEvent> = None;
+        EVENT_LOOP.with(|event_loop| {
+            event_loop
+                .borrow_mut()
+                .pump_events(Some(Duration::ZERO), |event, elwt| match event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        window_id,
+                        ..
+                    } => {
+                        if window_id == this.window_id {
+                            lua_window_event = Some(LuaWindowEvent::Exit);
+                            callback.call::<_, ()>(LuaWindowEvent::Exit).unwrap();
+                            elwt.exit();
+                        }
+                    }
+                    Event::AboutToWait => {}
+                    _ => (),
+                });
+        });
 
-        loop {
-            let mut lua_window_event: Option<LuaWindowEvent> = None;
-            event_loop.pump_events(Some(Duration::ZERO), |event, elwt| match event {
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => {
-                    lua_window_event = Some(LuaWindowEvent::Exit);
-                    callback.call::<_, ()>(LuaWindowEvent::Exit).unwrap();
-                    elwt.exit();
-                }
+        tokio::time::sleep(Duration::from_millis(16)).await;
 
-                Event::AboutToWait => {}
-                Event::WindowEvent {
-                    event: WindowEvent::RedrawRequested,
-                    ..
-                } => {
-                    lua_window_event = Some(LuaWindowEvent::Redraw);
+        let lua_value = callback
+            .call::<_, LuaValue>(lua_window_event.unwrap_or(LuaWindowEvent::Nothing))
+            .unwrap();
 
-                    lua.app_data_mut::<LuaWindowState>()
-                        .unwrap()
-                        .window
-                        .request_redraw();
-                }
-                _ => (),
-            });
-
-            let lua_value = callback
-                .call::<_, LuaValue>(lua_window_event.unwrap_or(LuaWindowEvent::Nothing))
-                .unwrap();
-
-            if lua_value.is_boolean() && lua_value.as_boolean().unwrap() {
-                break;
-            }
-
-            tokio::time::sleep(Duration::from_millis(16)).await;
+        if lua_value.is_boolean() && lua_value.as_boolean().unwrap() {
+            break;
         }
-
-        Ok(())
-    })?;
-
-    let thread = lua.create_thread(func)?;
-    lua.push_thread_back(&thread, callback)?;
+    }
 
     Ok(())
 }
 
 async fn lua_window_run_script<'lua>(
     lua: &Lua,
+    this: &LuaWindow,
     (script, callback): (LuaValue<'lua>, Option<LuaFunction<'lua>>),
 ) -> LuaResult<()> {
     if let Some(script) = script.as_str() {
         let (send, mut receive) = tokio::sync::watch::channel("null".to_string());
 
-        let result = lua
-            .app_data_mut::<LuaWindowState>()
-            .unwrap()
-            .webview
-            .evaluate_script_with_callback(script, move |unknown| {
-                if send.receiver_count() == 0 {
-                    return;
-                }
+        WEBVIEWS.with(|webviews| {
+            let webviews = webviews.borrow();
 
-                if let Err(error) = send.send(unknown) {
-                    println!("{}", error);
-                };
-            });
-
-        if result.is_err() {
-            return Err(LuaError::RuntimeError("Failed to evaluate script".into()));
-        }
-
-        if let Some(callback) = callback {
-            let inner_lua = lua
-                .app_data_ref::<Weak<Lua>>()
-                .expect("Missing weak lua ref")
-                .upgrade()
-                .expect("Lua was dropped unexpectedly");
-            let key = lua.create_registry_value(callback)?;
-
-            lua.spawn_local(async move {
-                let mut receive_inner = receive.clone();
-                loop {
-                    let changed = receive_inner.changed().await;
-
-                    if changed.is_ok() {
-                        let inner_handler = inner_lua.registry_value::<LuaFunction>(&key).unwrap();
-                        let unknown = receive.borrow_and_update();
-
-                        inner_handler
-                            .call_async::<_, LuaValue>(unknown.clone().into_lua(&inner_lua))
-                            .await
-                            .unwrap();
-
-                        break;
+            if let Some(webview) = webviews.get(&this.window_id) {
+                let result = webview.evaluate_script_with_callback(script, move |unknown| {
+                    if send.receiver_count() == 0 {
+                        return;
                     }
-                }
-            })
-        }
 
-        Ok(())
+                    if let Err(error) = send.send(unknown) {
+                        println!("{}", error);
+                    };
+                });
+
+                if result.is_err() {
+                    return Err(LuaError::RuntimeError("Failed to evaluate script".into()));
+                }
+
+                if let Some(callback) = callback {
+                    let inner_lua = lua
+                        .app_data_ref::<Weak<Lua>>()
+                        .expect("Missing weak lua ref")
+                        .upgrade()
+                        .expect("Lua was dropped unexpectedly");
+                    let key = lua.create_registry_value(callback)?;
+
+                    lua.spawn_local(async move {
+                        let mut receive_inner = receive.clone();
+                        loop {
+                            let changed = receive_inner.changed().await;
+
+                            if changed.is_ok() {
+                                let inner_handler =
+                                    inner_lua.registry_value::<LuaFunction>(&key).unwrap();
+                                let unknown = receive.borrow_and_update();
+
+                                inner_handler
+                                    .call_async::<_, LuaValue>(unknown.clone().into_lua(&inner_lua))
+                                    .await
+                                    .unwrap();
+
+                                break;
+                            }
+                        }
+                    })
+                }
+            }
+
+            Ok(())
+        })
     } else {
         Err(LuaError::FromLuaConversionError {
             from: script.type_name(),
@@ -172,19 +168,22 @@ async fn lua_window_run_script<'lua>(
     }
 }
 
-fn lua_window_set_visible(lua: &Lua, visible: bool) -> LuaResult<()> {
-    lua.app_data_mut::<LuaWindowState>()
-        .unwrap()
-        .window
-        .set_visible(visible);
+fn lua_window_set_visible(_lua: &Lua, this: &LuaWindow, visible: bool) -> LuaResult<()> {
+    WINDOWS.with(|windows| {
+        let windows = windows.borrow();
+
+        if let Some(window) = windows.get(&this.window_id) {
+            window.set_visible(visible);
+        }
+    });
 
     Ok(())
 }
 
 impl LuaUserData for LuaWindow {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_function("process_events", lua_window_process_events);
-        methods.add_async_function("run_script", lua_window_run_script);
-        methods.add_function("set_visible", lua_window_set_visible);
+        methods.add_async_method("process_events", lua_window_process_events);
+        methods.add_async_method("run_script", lua_window_run_script);
+        methods.add_method("set_visible", lua_window_set_visible);
     }
 }

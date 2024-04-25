ff81c0e5711e67787b5918841fafae0c1d3622a7
Flow <highflowey@gmail.com>
2024-04-25 18:06:00 +0330

    close method for window

--- src/lune/builtins/winit/window/config.rs
+++ src/lune/builtins/winit/window/config.rs
@@ -10,6 +10,11 @@ pub struct LuaWindow {
 
 impl LuaUserData for LuaWindow {
     fn add_methods<'lua, M: LuaUserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method_mut("close", |_lua: &Lua, this: &mut Self, _: ()| {
+            this.window.set_visible(false);
+            Ok(())
+        });
+
         methods.add_meta_method(
             "__eq",
             |_lua: &Lua, this: &Self, other: LuaUserDataRef<'lua, Self>| {

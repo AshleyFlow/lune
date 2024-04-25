7fd80bab67714d8d8a401f64128783959e4baaa1
Flow <highflowey@gmail.com>
2024-04-25 20:23:28 +0330

    config for creating webviews
    * contains { url, init_script (optional) }

--- src/lune/builtins/winit/webview/config.rs
+++ src/lune/builtins/winit/webview/config.rs
@@ -7,3 +7,28 @@ pub struct LuaWebView {
 }
 
 impl LuaUserData for LuaWebView {}
+
+// LuaWebViewConfig
+pub struct LuaWebViewConfig {
+    pub init_script: Result<String, LuaError>,
+    pub url: String,
+}
+
+impl<'lua> FromLua<'lua> for LuaWebViewConfig {
+    fn from_lua(value: LuaValue<'lua>, _lua: &'lua Lua) -> LuaResult<Self> {
+        if let Some(config) = value.as_table() {
+            Ok(Self {
+                init_script: config.get("init_script"),
+                url: config
+                    .get("url")
+                    .expect("WebViewConfig is missing 'url' property"),
+            })
+        } else {
+            Err(LuaError::FromLuaConversionError {
+                from: value.type_name(),
+                to: "table",
+                message: None,
+            })
+        }
+    }
+}
--- src/lune/builtins/winit/webview/mod.rs
+++ src/lune/builtins/winit/webview/mod.rs
@@ -1,6 +1,6 @@
 pub mod config;
 
-use self::config::LuaWebView;
+use self::config::{LuaWebView, LuaWebViewConfig};
 use super::window::config::LuaWindow;
 use mlua::prelude::*;
 use std::rc::Rc;
@@ -11,13 +11,22 @@ pub fn create<'lua>(
     values: LuaMultiValue<'lua>,
 ) -> LuaResult<LuaAnyUserData<'lua>> {
     let field1 = values.get(0).expect("Parameter 1 is missing");
+    let field2 = values.get(1).expect("Parameter 2 is missing");
+
+    let config = LuaWebViewConfig::from_lua(field2.clone(), lua)?;
 
     if let Some(window) = field1.as_userdata() {
         let mut window = window.borrow_mut::<LuaWindow>()?;
-        let webview = WebViewBuilder::new(&window.window)
-            .with_url("https://roblox.com/")
-            .build()
-            .unwrap();
+        let mut webview_builder = WebViewBuilder::new(&window.window).with_url(config.url);
+
+        if let Ok(init_script) = config.init_script.clone() {
+            webview_builder = webview_builder.with_initialization_script(init_script.as_str());
+        }
+
+        let webview = webview_builder.build().unwrap();
+        if let Ok(init_script) = config.init_script {
+            webview.evaluate_script(init_script.as_str()).unwrap();
+        }
 
         let lua_webview = LuaWebView { webview };
         let rc_lua_webview = Rc::new(lua_webview);

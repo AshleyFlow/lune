a96374e490e636adf55a50b9c759a051d7bc943e
Flow <highflowey@gmail.com>
2024-04-25 13:33:47 +0330

    call event loop callback lua function

--- src/lune/builtins/winit/mod.rs
+++ src/lune/builtins/winit/mod.rs
@@ -1,13 +1,10 @@
-use std::{borrow::BorrowMut, cell::RefCell, time::Duration};
+use std::{cell::RefCell, time::Duration};
 
 use crate::lune::util::TableBuilder;
 use mlua::prelude::*;
-use mlua_luau_scheduler::LuaSpawnExt;
+use mlua_luau_scheduler::{IntoLuaThread, LuaSchedulerExt, LuaSpawnExt};
 use once_cell::sync::Lazy;
-use winit::{
-    event_loop::{EventLoop, EventLoopBuilder},
-    platform::pump_events::EventLoopExtPumpEvents,
-};
+use winit::{event_loop::EventLoopBuilder, platform::pump_events::EventLoopExtPumpEvents};
 
 // thread_local! {
 //     pub static EVENT_LOOP: RefCell<EventLoop<()>> = RefCell::new(EventLoopBuilder::new().build().unwrap());
@@ -41,24 +38,31 @@ pub fn create(lua: &Lua) -> LuaResult<LuaTable> {
     });
 
     TableBuilder::new(lua)?
-        .with_function("eventLoop", winit_event_loop)?
+        .with_async_function("eventLoop", winit_event_loop)?
         .build_readonly()
 }
 
-pub fn winit_event_loop(lua: &Lua, callback: LuaFunction<'_>) -> LuaResult<()> {
-    lua.spawn_local(async {
-        let mut listener = EVENT_LOOP_SENDER.subscribe();
+pub async fn winit_event_loop(lua: &Lua, callback: LuaFunction<'_>) -> LuaResult<()> {
+    let loop_function =
+        lua.create_async_function(|inner_lua, inner_callback: LuaFunction<'_>| async move {
+            let mut listener = EVENT_LOOP_SENDER.subscribe();
 
-        loop {
-            let changed = listener.changed().await;
+            loop {
+                let changed = listener.changed().await;
 
-            if changed.is_ok() {
-                println!("Received new signal");
+                if changed.is_ok() {
+                    let message = *listener.borrow_and_update();
+                    inner_callback.call::<_, ()>(message)?;
+                }
+
+                tokio::time::sleep(Duration::ZERO).await;
             }
 
-            tokio::time::sleep(Duration::ZERO).await;
-        }
-    });
+            Ok(())
+        })?;
+
+    let loop_thread = lua.create_thread(loop_function)?;
+    lua.push_thread_back(loop_thread, callback)?;
 
     Ok(())
 }

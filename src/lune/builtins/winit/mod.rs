50a306fbd7b3586dd147f6ea2409a1696eb8306f
Flow <highflowey@gmail.com>
2024-04-25 13:08:34 +0330

    channel for event loop

--- src/lune/builtins/winit/mod.rs
+++ src/lune/builtins/winit/mod.rs
@@ -1,12 +1,61 @@
-use std::cell::RefCell;
-use winit::event_loop::{EventLoop, EventLoopBuilder};
+use std::{borrow::BorrowMut, cell::RefCell, time::Duration};
+
 use crate::lune::util::TableBuilder;
 use mlua::prelude::*;
+use mlua_luau_scheduler::LuaSpawnExt;
+use once_cell::sync::Lazy;
+use winit::{
+    event_loop::{EventLoop, EventLoopBuilder},
+    platform::pump_events::EventLoopExtPumpEvents,
+};
 
 thread_local! {
     pub static EVENT_LOOP: RefCell<EventLoop<()>> = RefCell::new(EventLoopBuilder::new().build().unwrap());
 }
 
+pub static EVENT_LOOP_SENDER: Lazy<tokio::sync::watch::Sender<()>> =
+    Lazy::new(|| tokio::sync::watch::Sender::new(()));
+
 pub fn create(lua: &Lua) -> LuaResult<LuaTable> {
-    TableBuilder::new(lua)?.build_readonly()
+    lua.spawn_local(async {
+        loop {
+            EVENT_LOOP.with(|event_loop| {
+                event_loop.borrow_mut().pump_events(
+                    Some(Duration::ZERO),
+                    |event, elwt| match event {
+                        winit::event::Event::WindowEvent { window_id, event } => {
+                            EVENT_LOOP_SENDER.send(()).unwrap();
+                        }
+                        _ => {
+                            EVENT_LOOP_SENDER.send(()).unwrap();
+                        }
+                    },
+                );
+            });
+
+            tokio::time::sleep(Duration::ZERO).await;
+        }
+    });
+
+    TableBuilder::new(lua)?
+        .with_function("eventLoop", winit_event_loop)?
+        .build_readonly()
+}
+
+pub fn winit_event_loop(lua: &Lua, callback: LuaFunction<'_>) -> LuaResult<()> {
+    lua.spawn_local(async {
+        let mut listener = EVENT_LOOP_SENDER.subscribe();
+
+        loop {
+            let changed = listener.changed().await;
+
+            if changed.is_ok() {
+                println!("Received new signal");
+            }
+
+            tokio::time::sleep(Duration::ZERO).await;
+        }
+    });
+
+    Ok(())
 }

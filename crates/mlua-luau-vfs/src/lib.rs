c323fac4f2d501d015e769ec5830286f2c57e5de
AshleyFlow <highflowey@gmail.com>
2024-10-12 20:40:58 +0330

    base struct

--- crates/mlua-luau-vfs/src/lib.rs
+++ crates/mlua-luau-vfs/src/lib.rs
@@ -1,1 +1,23 @@
-struct VirtualFileSystem {}
+#[derive(Debug, Default)]
+pub struct VirtualFileSystem {}
+
+impl VirtualFileSystem {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /**
+    Attach the struct to the provided lua VM
+     */
+    pub fn attach(self, lua: &mlua::Lua) -> mlua::Result<()> {
+        if lua.app_data_ref::<Self>().is_some() {
+            Err(mlua::Error::runtime(
+                "This lua VM already has a VirtualFileSystem struct",
+            ))
+        } else {
+            lua.set_app_data(self);
+
+            Ok(())
+        }
+    }
+}
